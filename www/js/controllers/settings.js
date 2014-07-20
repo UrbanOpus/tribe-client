a0f5e7fc8ea93eb4aee6983c0f99758a7f91bea8
faide <michael@faide.net>
2014-07-20 12:20:39 -0700

    Use dropdown menus with preset time intervals instead of native time input fields.
    Should improve reliability (#22) and clarity (#29) of both mood and question notifications.

--- www/js/controllers/settings.js
+++ www/js/controllers/settings.js
@@ -33,17 +33,69 @@ angular.module('tribe.settings', ['ionic'])
             $scope.mood_notify = mood_notify_defaults;
         }
 
+        $scope.data = {};
+
+        $scope.data.times = [];
+        $scope.data.value = '12:00 AM';
+
+        var i, j, timeCount = 0;;
+
+        // AM times
+
+        for (i = 0; i < 12; i += 1) {
+            for (j = 0; j < 60; j += 15) {
+                $scope.data.times.push((i === 0 ? '12' : i) + ':' + (j === 0 ? '00' : j) + ' AM');
+                timeCount += 1;
+            }
+        }
+
+        // PM times
+
+        for (i = 0; i < 12; i += 1) {
+            for (j = 0; j < 60; j += 15) {
+                $scope.data.times.push((i === 0 ? '12' : i) + ':' + (j === 0 ? '00' : j) + ' PM');
+                timeCount += 1;
+            }
+        }
+
+        // detect closest time
+
+        var now = new Date(),
+            nowHour   = now.getHours(),
+            nowMinute = now.getMinutes(),
+            nowAMPM   = now.getHours() < 12 ? 'AM' : 'PM';
+
+        nowHour = nowHour % 12;
+        if (nowHour === 0) {
+            nowHour = 12;
+        }
+
+        // get the nextclosest 15 minute interval
+        nowMinute = ((((nowMinute / 15) | 0) + 1) * 15) % 60;
+        if (nowMinute === 0) {
+            nowMinute = '00';
+            nowHour += 1;
+        }
+
+        if (nowHour === 12) {
+            nowAMPM = (nowAMPM === 'AM') ? 'PM' : 'AM';
+        }
+
+        $scope.data.value = nowHour + ':' + nowMinute + ' ' + nowAMPM;
+
+        console.log($scope.data.times);
+
         $scope.updateSettings = function() {
             var onSuccess;
             // if someone cleared the input -- treat it as an implicit
             // disable by setting it to the default
             if ($scope.mood_notify.time == "") {
                 $scope.mood_notify.checked = mood_notify_defaults.checked;
-                $scope.mood_notify.time = mood_notify_defaults.time;
+                //$scope.mood_notify.time = mood_notify_defaults.time;
             }
             
             onSuccess = $scope.mood_notify.checked
-                && function () { $localNotificationService.scheduleMood($scope.mood_notify.time); };
+                && function () { $localNotificationService.scheduleMood($scope.data.value); };
 
             $localNotificationService.cancel(onSuccess);
 
--- www/js/controllers/welcome.js
+++ www/js/controllers/welcome.js
@@ -4,12 +4,79 @@ angular.module('tribe.welcome', [])
     .controller('WelcomeCtrl', function ($scope, $ionicPopup, $location, UserService, APIService) {
         $scope.data = {};
 
-        $scope.data.time = '12:00';
+
+        $scope.data.times = [];
+        $scope.data.time = '12:00 PM';
+
+        var i, j, timeCount = 0;;
+
+        // AM times
+
+        for (i = 0; i < 12; i += 1) {
+            for (j = 0; j < 60; j += 15) {
+                $scope.data.times.push((i === 0 ? '12' : i) + ':' + (j === 0 ? '00' : j) + ' AM');
+                timeCount += 1;
+            }
+        }
+
+        // PM times
+
+        for (i = 0; i < 12; i += 1) {
+            for (j = 0; j < 60; j += 15) {
+                $scope.data.times.push((i === 0 ? '12' : i) + ':' + (j === 0 ? '00' : j) + ' PM');
+                timeCount += 1;
+            }
+        }
+
+        $scope.logTime = function () {
+            console.log($scope.data.time)
+        }
+
+        // detect closest time
+
+        var now = new Date(),
+            nowHour   = now.getHours(),
+            nowMinute = now.getMinutes(),
+            nowAMPM   = now.getHours() < 12 ? 'AM' : 'PM';
+
+        nowHour = nowHour % 12;
+        if (nowHour === 0) {
+            nowHour = 12;
+        }
+
+        // get the nextclosest 15 minute interval
+        nowMinute = ((((nowMinute / 15) | 0) + 1) * 15) % 60;
+        if (nowMinute === 0) {
+            nowMinute = '00';
+            nowHour += 1;
+        }
+
+        if (nowHour === 12) {
+            nowAMPM = (nowAMPM === 'AM') ? 'PM' : 'AM';
+        }
+
+        $scope.data.time = nowHour + ':' + nowMinute + ' ' + nowAMPM;
+
+        console.log($scope.data.times);
+
+
+        //$scope.data.time = '12:00';
 
         $scope.notifyUser = function () {
+            console.log($scope.data.time);
+            var time = $scope.data.time.split(' '),
+                timeStr;
+            time[0] = time[0].split(':');
+            time[0][0] = parseInt(time[0][0]) % 12;
+            if (time[1] === 'PM') {
+                time[0][0] += 12;
+            }
+
+            timeStr = time[0][0] + ':' + time[0][1];
+            console.log(timeStr);
             var submission = {
                 uuid: UserService.get('uuid'),
-                notificationTime: $scope.data.time,
+                notificationTime: timeStr,
                 registrationID: UserService.get('registrationID')
             };
 
@@ -22,8 +89,8 @@ angular.module('tribe.welcome', [])
                 });
             }).error(function (error) {
                 $ionicPopup.alert({
-                    title: 'Error!',
-                    template: error
+                    title: 'Error',
+                    template: 'Could not connect to the server.  Please check your network connection.'
                 });
             });
         };
